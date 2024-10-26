ab1c17e72989d409096815dd0b48fe7d6fd38133
3manvb <126747039+3manvb@users.noreply.github.com>
2024-10-26 12:22:42 +0330

    Update IPCF-Worker.js

--- IPCF-Worker.js
+++ IPCF-Worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 
 //حتما یویو آیدی تغییر بدید
 //https://www.uuidgenerator.net/
-let userID = 'f1a234f6-c946-4024-ba79-882f58c22727';
+let userID = '4863417f-1aa3-49eb-b1b4-4cbf4d6be80d';
 
 const ProxyIPs = [
 	'cdn.xn--b6gac.eu.org',
